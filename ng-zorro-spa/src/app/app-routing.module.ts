a485dff2f853701f2d83b09f460fec8a882d8fe8
inimeve <inigo.merino.vegas@gmail.com>
2021-03-28 03:17:31 +0200

    add no dynamic component

--- ng-zorro-spa/src/app/app-routing.module.ts
+++ ng-zorro-spa/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'goodbye', loadChildren: () => import('./pages/goodbye/goodbye.module').then(m => m.GoodbyeModule) }
+  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'pages' },
+  { path: '**', redirectTo: 'pages' }
+
 ];
 
 @NgModule({
