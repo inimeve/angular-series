fe5998a365692e3b99695165a5a3eaf58c43ac7e
inimeve <inigo.merino.vegas@gmail.com>
2021-04-10 18:42:37 +0200

    add layout components

--- ng-zorro-spa/src/app/ui/layout/layout.module.ts
+++ ng-zorro-spa/src/app/ui/layout/layout.module.ts
@@ -1,6 +1,9 @@
 import { NgModule, Type } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { BasicLayoutComponent } from './basic-layout/basic-layout.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 const COMPONENTS: Type<any>[] = [
   BasicLayoutComponent
@@ -11,6 +14,6 @@ const COMPONENTS: Type<any>[] = [
     SharedModule
   ],
   exports: [...COMPONENTS],
-  declarations: [...COMPONENTS]
+  declarations: [...COMPONENTS, HeaderComponent, SidebarComponent, FooterComponent]
 })
 export class LayoutModule { }
